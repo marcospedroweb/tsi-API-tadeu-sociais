6a666516c98865163fd2ce0b090e9d4329364693
Marcos <king.frontweb@outlook.com>
2022-06-02 23:11:06 -0300

    Desenvolvendo sistema de post de video e audio

--- js/modules/post.js
+++ js/modules/post.js
@@ -14,12 +14,15 @@ function createPost(type) {
     case 'image':
       //Criando elementos
       // Vai ter que pegar o preview do src para isso funcionar
-      element = document.createElement('img');
-      content = document.querySelector(`#post-content-${type}`).value.replace(/[\\"]/g, '');
-      content = content.replace('C:fakepath', '');
-      //Adicionando conteudo e classes
-      element.src = content;
-      element.classList.add('img-fluid');
+
+
+
+      // element = document.createElement('img');
+      // content = document.querySelector(`#post-content-${type}`).value.replace(/[\\"]/g, '');
+      // content = content.replace('C:fakepath', '');
+      // //Adicionando conteudo e classes
+      // element.src = content;
+      // element.classList.add('img-fluid');
       break;
     case 'video':
       //Criando elementos
@@ -78,3 +81,71 @@ if (btnsTadeuzar && postDiv)
       divPosts.appendChild(element);
     });
   });
+
+
+function readImage() {
+  if (this.files && this.files[0]) {
+    var file = new FileReader();
+    file.onload = function (event) {
+      console.log(event.target.result.length);
+      let element = document.createElement('img');
+      let content = event.target.result;
+      //Adicionando conteudo e classes
+      element.src = content;
+      element.classList.add('img-fluid');
+      const divPosts = document.querySelector('#post-test');
+      divPosts.appendChild(element);
+    };
+    file.readAsDataURL(this.files[0]);
+  }
+
+}
+
+const inputImage = document.querySelector('#post-content-image');
+const inputVideo = document.querySelector('#post-content-video');
+const inputAudio = document.querySelector('#post-content-audio');
+const divPosts = document.querySelector('#post-test');
+
+function readFile() {
+  if (this.files && this.files[0]) {
+    const file = new FileReader();
+    file.onload = function (event) {
+      let element = document.createElement('img');
+      let content = event.target.result;
+      //Adicionando conteudo e classes
+      element.src = content;
+      element.classList.add('img-fluid');
+      divPosts.appendChild(element);
+    };
+    file.readAsDataURL(this.files[0]);
+  }
+}
+
+inputImage.addEventListener('change', readFile);
+
+inputVideo.addEventListener('change', event => {
+  let file = event.target.files[0];
+  let blobURL = URL.createObjectURL(file);
+  //Criando elementos
+  let element = document.createElement('video');
+  let source = document.createElement('source');
+  //Adicionando conteudo e classes
+  source.src = blobURL;
+  element.setAttribute('controls', 'controls');
+  element.appendChild(source);
+  element.classList.add('ratio', 'ratio-16x9');
+  divPosts.appendChild(element);
+});
+
+inputVideo.addEventListener('load', event => {
+  let file = event.target.files[0];
+  let blobURL = URL.createObjectURL(file);
+  console.log(blobURL);
+});
+
+inputAudio.addEventListener('change', event => {
+  const source = inputAudio;
+  source[0].src = URL.createObjectURL(this.files[0]);
+  source.parent()[0].load();
+  console.log(source);
+});
